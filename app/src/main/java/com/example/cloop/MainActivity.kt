bae916d941460315f9cd9c6f1be410e4537ad51c
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-04-11 12:19:09 +0900

    [#2] Feat: 	화면 이동 및 바텀바 클릭 연동

--- app/src/main/java/com/example/cloop/MainActivity.kt
+++ app/src/main/java/com/example/cloop/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.cloop
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.cloop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(binding.root)
+        setupBottomNavigation()
+
+    }
+
+    // 하단 네비게이션 설정
+    private fun setupBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvMain.setupWithNavController(navController)
+        setBottomNaviVisible(navController)
+    }
+
+    // 하단바 표시 여부 설정
+    private fun setBottomNaviVisible(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnvMain.visibility = when (destination.id) {
+                R.id.fragment_home, R.id.fragment_closet, R.id.fragment_donate, R.id.fragment_mypage -> View.VISIBLE
+                else -> View.GONE
+            }
         }
     }
 }
