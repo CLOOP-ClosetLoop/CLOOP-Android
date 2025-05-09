6dcd837d02c491c89cf8d9f792d1cc4b9307855c
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-05-09 13:41:33 +0900

    [#13] Feat: 뒤로가기

--- app/src/main/java/com/example/cloop/ui/closet/InactiveClosetFragment.kt
+++ app/src/main/java/com/example/cloop/ui/closet/InactiveClosetFragment.kt
@@ -27,11 +27,6 @@ class InactiveClosetFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         // 뒤로가기
         binding.ivBack.setOnClickListener {
--- app/src/main/java/com/example/cloop/ui/home/OutfitRegisterFragment.kt
+++ app/src/main/java/com/example/cloop/ui/home/OutfitRegisterFragment.kt
@@ -1,21 +1,45 @@
 package com.example.cloop.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.cloop.R
+import com.example.cloop.databinding.FragmentOutfitRegisterBinding
 
 class OutfitRegisterFragment : Fragment() {
 
+    private var _binding: FragmentOutfitRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: OutfitRegisterFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_outfit_register, container, false)
+    ): View {
+        _binding = FragmentOutfitRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val selectedDate = args.selectedDate
+        Log.d("받은날짜", selectedDate)  // ex: "2025-04-23"
+
+        binding.ivBack.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
