68ab22360a662a38b2afd26c5a699fbe505be302
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-05-11 21:47:36 +0900

    [#18] Feat: 옷 등록 API Retrofit 인터페이스 정의

--- app/src/main/java/com/example/cloop/data/remote/ClothService.kt
+++ app/src/main/java/com/example/cloop/data/remote/ClothService.kt
@@ -1,8 +1,11 @@
 package com.example.cloop.data.remote
 
+import com.example.cloop.data.model.cloth.ClothRegisterRequest
 import com.example.cloop.data.model.cloth.ImageUploadResponse
+import com.example.cloop.data.model.cloth.RegisterClothResponse
 import okhttp3.MultipartBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -10,11 +13,19 @@ import retrofit2.http.Part
 
 interface ClothService {
 
+    // 옷 이미지 업로드
     @Multipart
     @POST("clothes/image")
     suspend fun uploadClothImage(
         @Header("Authorization") token: String,
         @Part image: MultipartBody.Part
     ): Response<ImageUploadResponse>
+
+    // 옷 정보 등록
+    @POST("clothes")
+    suspend fun registerCloth(
+        @Header("Authorization") token: String,
+        @Body request: ClothRegisterRequest
+    ): Response<RegisterClothResponse>
 
 }
