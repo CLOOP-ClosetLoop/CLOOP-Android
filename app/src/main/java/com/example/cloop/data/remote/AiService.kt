ff6959a192b637ce5963da10c4dc66dca24c28e7
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-05-12 01:30:05 +0900

    [#20] Fix: 헤더 추가

--- app/src/main/java/com/example/cloop/data/remote/AiService.kt
+++ app/src/main/java/com/example/cloop/data/remote/AiService.kt
@@ -3,11 +3,13 @@ package com.example.cloop.data.remote
 import com.example.cloop.data.model.cloth.AiResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AiService {
     @POST("/clothes/ai")
     suspend fun classifyCloth(
+        @Header("Authorization") token: String,
         @Body imageUrlBody: Map<String, String>
     ): Response<AiResponse>
 }
