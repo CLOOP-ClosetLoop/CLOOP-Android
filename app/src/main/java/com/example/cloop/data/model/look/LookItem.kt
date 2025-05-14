805d3e51cc75b047d5a4a9551a8ccc9ef6cd54f8
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-05-14 23:49:48 +0900

    [#30] Feat: 착장 조회 API 설정 및 데이터 모델 생성

---
+++ app/src/main/java/com/example/cloop/data/model/look/LookItem.kt
@@ -0,0 +1,7 @@
+package com.example.cloop.data.model.look
+
+data class LookItem(
+    val lookId: Int,
+    val createdAt: String,
+    val imageUrl: String
+)
--- app/src/main/java/com/example/cloop/data/remote/LookService.kt
+++ app/src/main/java/com/example/cloop/data/remote/LookService.kt
@@ -1,15 +1,18 @@
 package com.example.cloop.data.remote
 
+import com.example.cloop.data.model.look.LookItem
 import com.example.cloop.data.model.look.LookRegisterResponse
 import com.example.cloop.data.model.look.LookRequest
 import com.example.cloop.data.model.look.UploadResponse
 import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface LookService {
 
@@ -26,4 +29,11 @@ interface LookService {
         @Header("Authorization") token: String,
         @Body lookData: LookRequest
     ): Response<LookRegisterResponse>
+
+
+    @GET("looks")
+    suspend fun getLooksByDate(
+        @Header("Authorization") token: String,
+        @Query("date") date: String
+    ): Response<List<LookItem>>
 }
