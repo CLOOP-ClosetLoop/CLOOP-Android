34d93a287d7052e66d35513224ec67954f5de6a2
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-05-14 23:52:29 +0900

    [#30] Feat: Look 뷰모델 생성

--- app/src/main/java/com/example/cloop/presentation/home/viewmodel/LookViewModel.kt
+++ app/src/main/java/com/example/cloop/presentation/home/viewmodel/LookViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.cloop.presentation.home.viewmodel
 
-class LookViewModel {
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cloop.TokenManager
+import com.example.cloop.data.model.look.LookItem
+import com.example.cloop.data.remote.LookService
+import kotlinx.coroutines.launch
+
+class LookViewModel(private val lookService: LookService) : ViewModel() {
+
+    private val _looks = MutableLiveData<List<LookItem>>()
+    val looks: LiveData<List<LookItem>> get() = _looks
+
+    fun fetchLooksByDate(context: Context, date: String) {
+        val token = TokenManager.getAccessToken(context)
+        if (token == null) {
+            return
+        }
+
+        viewModelScope.launch {
+            try {
+                val response = lookService.getLooksByDate("Bearer $token", date)
+                if (response.isSuccessful) {
+                    _looks.value = response.body() ?: emptyList()
+                } else {
+                    Log.e("LookViewModel", "API Error: ${response.code()} ${response.message()}")
+                }
+            } catch (e: Exception) {
+                Log.e("LookViewModel", "Exception: ${e.localizedMessage}")
+            }
+        }
+    }
 }
