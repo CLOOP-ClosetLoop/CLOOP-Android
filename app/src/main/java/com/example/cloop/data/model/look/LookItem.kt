4bd031f3366e33742e43d3496d381e409d93c4fa
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2025-05-15 00:54:41 +0900

    [#30] Fix: 데이터 구조 변경

--- app/src/main/java/com/example/cloop/data/model/look/LookItem.kt
+++ app/src/main/java/com/example/cloop/data/model/look/LookItem.kt
@@ -1,7 +1,10 @@
 package com.example.cloop.data.model.look
 
+import com.example.cloop.data.model.cloth.Cloth
+
 data class LookItem(
     val lookId: Int,
     val createdAt: String,
-    val imageUrl: String
+    val imageUrl: String,
+    val clothes: List<Cloth>
 )
